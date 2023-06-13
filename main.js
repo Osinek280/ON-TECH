7ccbdf16737209c9d166e3a368616a1ff6a5c23d
Szymon <szymon.osinski.2009@gmail.com>
2023-06-13 12:49:44 +0200

    responsive view

--- main.js
+++ main.js
@@ -4,40 +4,6 @@ const menuMark = document.querySelectorAll('.hamburger-menu ul li')
 
 let isOpen = false;
 
-document.querySelectorAll('.mark').forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        if(e.target.textContent === 'HOME')
-        {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
-        else if(e.target.textContent === 'CLASSIC') {
-            const section = document.querySelector('.classic-container');
-
-            window.scrollTo({
-                top: section.offsetTop - 100,
-                behavior: 'smooth'
-            });
-
-        }
-        else if(e.target.textContent === 'SOFT LINE') {
-            const section = document.querySelector('.soft-container')
-            
-            window.scrollTo({
-                top: section.offsetTop - 100,
-                behavior: 'smooth'
-            });
-        }
-        else if(e.target.textContent === 'KONTAKT') {
-            const section = document.querySelector('.contact')
-            
-            window.scrollTo({
-                top: section.offsetTop - 100,
-                behavior: 'smooth'
-            });
-        }
-    })
-})
-
 menuBtn.addEventListener('click', (e) => {
     isOpen = !isOpen;
     menu.classList.toggle('active')
